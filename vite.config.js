1d22d14efd49a835edc1bf70d8baf2eaae631b88
Scheaux <125101@inbox.ru>
2022-05-27 19:49:44 +0700

    final

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,4 @@
 import { defineConfig } from 'vite';
-import path from 'path';
-import alias from '@rollup/plugin-alias';
-
-const projectRootDir = path.resolve(__dirname);
-
-// export default defineConfig({
-//   plugins: [
-//     alias({
-//       entries: [
-//         {
-//           find: '@',
-//           replacement: path.resolve(projectRootDir, 'src'),
-//         },
-//       ],
-//     }),
-//   ],
-//   root: 'src',
-//   build: {
-//     outDir: '../dist',
-//   },
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(projectRootDir, 'src'),
-//     },
-//   },
-// });
 
 export default defineConfig({
   base: '/vite-project/',
