5838002fa8fa5a3ac700536c65376ddc38bcaeb1
Scheaux <125101@inbox.ru>
2022-05-27 18:51:13 +0700

    path resolve fix

--- vite.config.js
+++ vite.config.js
@@ -4,24 +4,32 @@ import alias from '@rollup/plugin-alias';
 
 const projectRootDir = path.resolve(__dirname);
 
+// export default defineConfig({
+//   plugins: [
+//     alias({
+//       entries: [
+//         {
+//           find: '@',
+//           replacement: path.resolve(projectRootDir, 'src'),
+//         },
+//       ],
+//     }),
+//   ],
+//   root: 'src',
+//   build: {
+//     outDir: '../dist',
+//   },
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(projectRootDir, 'src'),
+//     },
+//   },
+// });
+
 export default defineConfig({
-  plugins: [
-    alias({
-      entries: [
-        {
-          find: '@',
-          replacement: path.resolve(projectRootDir, 'src'),
-        },
-      ],
-    }),
-  ],
+  base: `/${__dirname}`,
   root: 'src',
   build: {
     outDir: '../dist',
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(projectRootDir, '/src'),
-    },
   },
 });
