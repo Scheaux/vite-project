b2466bbe4636310225c3dae0e52bd5e72b56f1b9
Scheaux <125101@inbox.ru>
2022-05-27 18:35:31 +0700

    env fixes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,27 @@
 import { defineConfig } from 'vite';
+import path from 'path';
+import alias from '@rollup/plugin-alias';
+
+const projectRootDir = path.resolve(__dirname);
 
 export default defineConfig({
+  plugins: [
+    alias({
+      entries: [
+        {
+          find: '@',
+          replacement: path.resolve(projectRootDir, 'src'),
+        },
+      ],
+    }),
+  ],
   root: 'src',
   build: {
     outDir: '../dist',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './dist'),
+    },
   },
 });
