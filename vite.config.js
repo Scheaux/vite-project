dc23afaf209acd4424d0942ea262496f1f2ddd74
Scheaux <125101@inbox.ru>
2022-05-27 18:24:04 +0700

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite';
-import babel from 'vite-plugin-babel';
 
 export default defineConfig({
-  plugins: [
-    // Babel will try to pick up Babel config files (.babelrc or .babelrc.json)
-    babel({
-      babelConfig: {
-        babelrc: true,
-        configFile: false,
-        only: ['./src'],
-      },
-    }),
-  ],
+  root: 'src',
+  build: {
+    outDir: '../dist',
+  },
 });
